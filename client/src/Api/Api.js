eb3cb3e3dc120950bfe6460e1dba26e38cbe6269
Soumen Samanta <samantasoumen8978@gmail.com>
2025-07-18 13:04:31 +0530

    push

--- client/src/Api/Api.js
+++ client/src/Api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios instance
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://fitessence-2.onrender.com',
   withCredentials: true, // optional: use for sending cookies if needed
 });
 
